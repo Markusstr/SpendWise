73b9f3c4a2a5522f69aa878f9057b77b3282008b
Markusstr <markus.strandman@gmail.com>
2019-11-16 01:08:11 +0200

    Added product activity

--- app/src/main/java/com/example/spendwise/ProductActivity.java
+++ app/src/main/java/com/example/spendwise/ProductActivity.java
@@ -1,8 +1,11 @@
 package com.example.spendwise;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class ProductActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class ProductActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("Product");
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            finish();
+        }
+        return true;
     }
 }
