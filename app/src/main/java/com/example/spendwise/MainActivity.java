6c8f49d9eb9370b8da6b7b84fd844a767243a253
Markusstr <markus.strandman@gmail.com>
2019-11-16 20:01:10 +0200

    Recycler view in mainactivity

--- app/src/main/java/com/example/spendwise/MainActivity.java
+++ app/src/main/java/com/example/spendwise/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.spendwise;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import android.view.MenuItem;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.navigation.ui.AppBarConfiguration;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
@@ -17,13 +20,23 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.Menu;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerViewAdapter mAdapter;
+    private ArrayList<DataClass> dataList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +49,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         final ImageButton main_url_close = findViewById(R.id.main_url_close);
         final EditText main_url_input = findViewById(R.id.main_url_input);
         final Button main_url_button = findViewById(R.id.main_url_button);
+        dataList = new ArrayList<>();
 
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -69,6 +83,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             }
         });
 
+        Bitmap product1 = BitmapFactory.decodeResource(getResources(), R.drawable.product1);
+        Bitmap product2 = BitmapFactory.decodeResource(getResources(), R.drawable.product2);
+        Bitmap product3 = BitmapFactory.decodeResource(getResources(), R.drawable.product3);
+        Bitmap product4 = BitmapFactory.decodeResource(getResources(), R.drawable.product4);
+        DataClass data;
+
+        data = new DataClass(product1, "Camera", "Price: 190€");
+        dataList.add(data);
+        data = new DataClass(product2, "Microphone", "Price: 60€");
+        dataList.add(data);
+        data = new DataClass(product3, "Boat", "Price: 3000€");
+        dataList.add(data);
+        data = new DataClass(product4, "Handheld drill", "Price: 150€");
+        dataList.add(data);
+
+        createRecyclerView();
+
+
 
 
     }
@@ -87,5 +119,40 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         //TODO: Check what id is
         return true;
+    }
+
+    /*@Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK) {
+                assert data != null;
+                String product_name = data.getStringExtra("product_name");
+                String product_price = data.getStringExtra("product_price");
+                Bitmap product_image = data.getExtra
+
+            }
+        }
+    }*/
+
+    public void createRecyclerView() {
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        mAdapter = new RecyclerViewAdapter(dataList);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mAdapter.setOnItemClickListener(new RecyclerViewAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+            }
+
+            @Override
+            public void onCloseClick(int position) {
+                dataList.remove(position);
+                mAdapter.notifyItemRemoved(position);
+            }
+        });
     }
 }
